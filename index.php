bd4f975e7f08db30241062a54e70abbf977e2af3
Yurienu <yurienu@myopera.com>
2012-11-21 16:32:49 +0100

    XHTML 5 + standards

--- index.php
+++ index.php
@@ -17,79 +17,82 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-### Initialisation
+### Init
 $xml = simplexml_load_file('fql.xml');
 $sex = array('male' => 0, 'female' => 0);
 $sexPercent = array('male' => 0, 'female' => 0);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="content-style-type" content="text/css" />
-        <meta http-equiv="content-language" content="english" />
-        <meta name="robots" content="noindex,nofollow" />
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <meta charset="utf-8" />
 
-        <title>Facebook stats</title>
+  <title>Facebook stats</title>
+  <meta name="description" content="Facebook stats shows you some useless stats about your friends on Facebook which you can’t see on the website" />
+  <meta name="keywords" content="facebook, stats" />
+  <meta name="author" content="Julien Papasian" />
+  <meta name="robots" content="noindex, nofollow" />
 
-        <link href="style.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <h2>Facebook stats</h2>
+  <link rel="stylesheet" title="Stats" type="text/css" href="style.css" />
+</head>
+<body>
+  <header><h1>Facebook stats</h1></header>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th>Mutual friends</th>
-                    <th>Wall count</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 0;
-            foreach($xml->children() as $profile)
-            {
-                ++$i;
+  <div id="content">
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Mutual friends</th>
+          <th>Wall count</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+      $i = 0;
+      foreach($xml->children() as $profile)
+      {
+        ++$i;
 
-                $percentMutual = (intval($profile->friend_count) != 0) ? round(($profile->mutual_friend_count / $profile->friend_count) * 100, 2) : 0;
+        $percentMutual = (intval($profile->friend_count) != 0) ? round(($profile->mutual_friend_count / $profile->friend_count) * 100, 2) : 0;
 
-                echo '<tr'.(($percentMutual > 15) ? ' style="background-color: #FFFAAA;"' : '').'>
-                    <td class="number centre">'.$i.'</td>
-                    <td class="pic_square centre"><a href="'.$profile->profile_url.'"><img src="'.$profile->pic_square.'" alt="" /></a></td>
-                    <td class="name"><a href="'.$profile->profile_url.'">'.$profile->name.'</a></td>
-                    <td class="friends centre">'.$profile->mutual_friend_count.' / '.((intval($profile->friend_count) != 0) ? $profile->friend_count.'<br />('.$percentMutual.'%)' : '—').'</td>
-                    <td class="wall centre">'.((intval($profile->wall_count) != 0) ? $profile->wall_count : '—').'</td>
-                </tr>';
+        echo '<tr'.(($percentMutual > 15) ? ' style="background-color: #FFFAAA;"' : '').'>
+          <td>'.$i.'</td>
+          <td><a href="'.$profile->profile_url.'"><img src="'.$profile->pic_square.'" alt="" /></a></td>
+          <td class="name"><a href="'.$profile->profile_url.'">'.$profile->name.'</a></td>
+          <td>'.$profile->mutual_friend_count.' / '.((intval($profile->friend_count) != 0) ? $profile->friend_count.'<br />('.$percentMutual.'%)' : '—').'</td>
+          <td>'.((intval($profile->wall_count) != 0) ? $profile->wall_count : '—').'</td>
+        </tr>';
 
-                if($profile->sex != '') ++$sex[strval($profile->sex)];
-            }
-            ?>
-            </tbody>
-        </table>
+        if($profile->sex != '') ++$sex[strval($profile->sex)];
+      }
+      ?>
+      </tbody>
+    </table>
 
-        <p>&nbsp;</p>
+    <hr />
 
-        <table>
-            <thead><tr><th colspan="3">Sex distribution</th></tr></thead>
-            <tbody>
-                <tr>
-                    <?php
-                    $sexPercent['male'] = round($sex['male'] * 100 / $i, 2);
-                    $sexPercent['female'] = round($sex['female'] * 100 / $i, 2);
+    <table>
+      <thead><tr><th colspan="3">Sex distribution</th></tr></thead>
+      <tbody>
+        <tr>
+          <?php
+          $sexPercent['male'] = round($sex['male'] * 100 / $i, 2);
+          $sexPercent['female'] = round($sex['female'] * 100 / $i, 2);
 
-                    echo '<td>'.$sex['male'].' ('.$sexPercent['male'].'%) male</td>
-                    <td>'.$sex['female'].' ('.$sexPercent['female'].'%) female</td>
-                    <td>'.intval($i - ($sex['male'] + $sex['female'])).' ('.round(100 - ($sexPercent['male'] + $sexPercent['female']), 2).'%) not specified</td>';
-                    ?>
-                </tr>
-            </tbody>
-        </table>
+          echo '<td>'.$sex['male'].' ('.$sexPercent['male'].'%) male</td>
+          <td>'.$sex['female'].' ('.$sexPercent['female'].'%) female</td>
+          <td>'.intval($i - ($sex['male'] + $sex['female'])).' ('.round(100 - ($sexPercent['male'] + $sexPercent['female']), 2).'%) not specified</td>';
+          ?>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
-        <p>&nbsp;</p>
+  <hr />
 
-        <p class="centre">Copyright © 2012 <a href="https://github.com/Yurienu/FacebookStats">Facebook Stats</a></p>
-    </body>
+  <footer><p>Copyright © 2012 <a href="https://github.com/Yurienu/FacebookStats">Facebook stats</a></p></footer>
+</body>
 </html>
